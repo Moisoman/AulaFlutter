4af506573e2f914e40f8be0756c08eb5c8f6d22a
Moises <moisespedromonteiro@gmail.com>
2026-03-23 21:56:43 -0300

    aula 23-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,100 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(const MeuApp());
-}
-
-class MeuApp extends StatelessWidget{
-  const MeuApp({super.key});
-
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: "Titulo",
-      home: const MinhaHomePage(title: "Testando"),
-      );
-
-  }
-}
-
-class MinhaHomePage extends StatefulWidget{
-  const MinhaHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MinhaHomePage> createState() => _MinhaHomePageState();
-
-}
-
-class _MinhaHomePageState extends State<MinhaHomePage>{
   int _counter = 0;
-
-  void incrementar(){
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: .center,
-          children: [
-            const Text("Quantidade de clicks:"),
-            Text('$_counter')
-          ],
+    runApp(MaterialApp(
+      home:Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child:  Text("xingxong")
+          ),
+          backgroundColor: Color.fromARGB(122, 201, 99, 37),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementar,
-        child: const Icon(Icons.add), 
+        body: Center(
+          child: Row(
+            mainAxisAlignment: .center,
+            children: [
+              Text('$_counter'),
+              FloatingActionButton(onPressed: (){ _counter++;},
+              child: const Icon(Icons.add),
+              ),
+              Column(
+                mainAxisAlignment: .center,
+                children: [
+                  const Text("COLUNA"),
+                  Row(
+                    mainAxisAlignment: .end,
+                    children: [
+                      const Text("LINHA")
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+          
         ),
-    );
-  }
-
-
+        
+      )
+    )
+  );
 }
+  
+
+// class MeuApp extends StatelessWidget{
+//   const MeuApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context){
+//     return MaterialApp(
+//       title: "Titulo",
+//       home: const MinhaHomePage(title: "Testando"),
+//       );
+
+//   }
+// }
+
+// class MinhaHomePage extends StatefulWidget{
+//   const MinhaHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MinhaHomePage> createState() => _MinhaHomePageState();
+
+// }
+
+// class _MinhaHomePageState extends State<MinhaHomePage>{
+//   int _counter = 0;
+
+//   void incrementar(){
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context){
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: .center,
+//           children: [
+//             const Text("Quantidade de clicks:"),
+//             Text('$_counter')
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: incrementar,
+//         child: const Icon(Icons.add), 
+//         ),
+//     );
+//   }
+
+
+// }
---
+++ lib/main2.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget{
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "Titulo",
+      home: const MinhaHomePage(title: "Testando"),
+      );
+
+  }
+}
+
+class MinhaHomePage extends StatefulWidget{
+  const MinhaHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MinhaHomePage> createState() => _MinhaHomePageState();
+
+}
+
+class _MinhaHomePageState extends State<MinhaHomePage>{
+  int _counter = 0;
+
+  void incrementar(){
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text("Quantidade de clicks:"),
+            Text('$_counter')
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: incrementar,
+        child: const Icon(Icons.add), 
+        ),
+    );
+  }
+
+
+}
